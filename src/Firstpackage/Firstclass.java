ac9b115e65aae8460055d77a5cf2cbefb2a15599
alilsamzey <alilsamzey@gmail.com>
2020-11-08 17:11:18 +0100

    second commmit

--- src/Firstpackage/Firstclass.java
+++ src/Firstpackage/Firstclass.java
@@ -3,8 +3,11 @@ package Firstpackage;
 public class Firstclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("First commit from local maste branch");
+		
+		
+		System.out.println(" after kommit");
 	}
 
 }
